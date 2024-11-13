bb8c535b06a60390f4eb5d5e2c2dc6bb9b004677
Caio Duque <caioduque.dev@gmail.com>
2024-11-13 19:36:19 -0300

    added features

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import KeyIcon from "@mui/icons-material/Key";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
@@ -11,17 +12,23 @@ export default function Login() {
           <div className="icon-container">
             <AlternateEmailIcon />
           </div>
-          <input type="text" placeholder="usuário" />
+          <input type="text" placeholder="Usuário" />
         </div>
         <div className="input-container">
           <KeyIcon />
-          <input type="text" placeholder="senha" />
+          <input type="password" placeholder="Senha" />
         </div>
         <div className="remember-be">
           <input type="checkbox" name="rememberMe" id="" />
           <label htmlFor="rememberMe">Lembre-se de mim</label>
         </div>
+
         <button>Entrar</button>
+      </div>
+      <div className="not-a-member">
+        <p>
+          Ainda não é membro? Registre-se <Link to={"/login"}>aqui</Link>
+        </p>
       </div>
     </div>
   );
